914ef98b568700bf5213044fb5cd89c39efb03a1
Michal Cichra <mikz@users.noreply.github.com>
2017-05-05 08:05:21 +0200

    [luarocks] allow installing compiled extensions
    that depend on luajit headers (like luafilesystem)

--- config-5.1.lua
+++ config-5.1.lua
@@ -1,3 +1,8 @@
 rocks_trees = {
    { name = [[openresty]], root = [[/usr/local/openresty/luajit]] }
 }
+
+variables = {
+  LUA_INCDIR = [[/usr/local/openresty/luajit/include/luajit-2.1]],
+  LUA_LIBDIR = [[/usr/local/openresty/luajit/lib]]
+}
