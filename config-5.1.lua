c3db77ffe4a963a309959202a69ee5ba49f11112
Michal Cichra <michal@cichra.cz>
2017-05-19 14:44:33 +0200

    fix installing compiled rocks

--- config-5.1.lua
+++ config-5.1.lua
@@ -3,3 +3,10 @@ lua_interpreter = [[resty]]
 rocks_trees = {
    { name = [[openresty]], root = [[/usr/local/openresty/luajit]] }
 }
+
+lib_modules_path = [[/lib/lua/]]..lua_version
+
+variables = {
+  LUA_INCDIR = [[/usr/local/openresty/luajit/include/luajit-2.1]],
+  LUA_LIBDIR = [[/usr/local/openresty/luajit/lib]]..lua_version
+}
