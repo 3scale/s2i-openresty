75deb4108af92a51186ef968f1d14ae5d8ee2871
Michal Cichra <michal@cichra.cz>
2017-11-01 12:50:45 +0100

    use luarocks site config fo global settings
    this allows luarocks to install another luarocks
    and using this config in the runtime image allows
    us to copy luarocks over to the runtime image

--- config-5.1.lua
+++ config-5.1.lua
@@ -1,16 +1,6 @@
 lua_interpreter = [[resty]]
-
-rocks_trees = {
-   {
-      name = [[openresty]],
-      root = [[/usr/local/openresty/luajit]],
-      bin_dir = os_getenv('LUA_BIN_PATH'),
-   }
-}
-
 lib_modules_path = [[/lib/lua/]]..lua_version
 
 variables = {
-  LUA_INCDIR = [[/usr/local/openresty/luajit/include/luajit-2.1]],
-  LUA_LIBDIR = [[/usr/local/openresty/luajit/lib]]..lua_version
+	LUAROCKS_PREFIX = [[/usr]]
 }
