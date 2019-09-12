2766eccaa284e69b3517be43085c40f70c217da5
Eloy Coto <eloy.coto@gmail.com>
2019-09-12 11:21:05 +0200

    S2I install is version mismatch
    Signed-off-by: Eloy Coto <eloy.coto@gmail.com>

--- travis/install-s2i.sh
+++ travis/install-s2i.sh
@@ -8,12 +8,27 @@ arch=$(uname -m | sed 's/x86_/amd/' | tr -d 'i' | sed 's/686/386/')
 version=$(echo "${S2I_VERSION}" | cut -f1 -d-)
 sha=$(echo "${S2I_VERSION}" | cut -f2 -d-)
 
-if [ ! -f "${PREFIX}/s2i" ]; then
+install_s2i(){
   cd /tmp/
   mkdir -p "${PREFIX}"
   wget -T 60 -c "https://github.com/openshift/source-to-image/releases/download/v${version}/source-to-image-v${version}-${sha}-${os}-${arch}.tar.gz" -O source-to-image.tar.gz
   tar -xzf source-to-image.tar.gz -C "${PREFIX}/"
   rm -rf source-to-image.tar.gz
-else
-  echo "Using cached s2i."
+}
+
+if [ ! -f "${PREFIX}/s2i" ]; then
+    echo "Installing s2i"
+    install_s2i
+    exit 0
 fi
+
+installed_version=$(s2i version | awk '{print substr($2,2)}')
+
+
+if [ "$installed_version" != "$version" ]; then
+    echo "Installing s2i because mismatched version current_version='$installed_version'"
+    # Installed a new version because it's not the same
+    install_s2i
+fi
+
+# Using installed version
